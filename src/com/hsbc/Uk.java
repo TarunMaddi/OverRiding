117ff439b4fdac4a5fead0578eef2d76cb6244ef
TarunMaddi <tarunmaddi369@gmail.com>
2023-10-19 22:28:15 +0530

    Override

---
+++ src/com/hsbc/Uk.java
@@ -0,0 +1,19 @@
+package com.hsbc;
+
+public class Uk extends Usa{
+	public void oilExport() {
+		super.oilExport();
+		System.out.println("Export oil to Ukraine");
+	}
+	public void oilPrice() {
+		super.oilPrice();
+		System.out.println("Oil price for a Barell is: 20000$");
+	}
+	public static void main(String [] args) {
+		Uk o=new Uk();
+		o.oilExport();
+		o.oilPrice();
+				
+	}
+
+}
---
+++ src/com/hsbc/Usa.java
@@ -0,0 +1,11 @@
+package com.hsbc;
+
+public class Usa {
+	public void oilExport() {
+		System.out.println("Export oil to india");
+	}
+	public void oilPrice() {
+		System.out.println("Oil price for a Barell is: 10000$");
+	}
+
+}
